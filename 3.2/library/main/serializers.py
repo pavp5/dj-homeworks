6108dcd8e3a6767c016810675e9170870e392d1b
Pavel Puganov <pavp5@yandex.ru>
2024-02-20 12:48:26 +0300

    Homework 3.2

---
+++ 3.2/library/main/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from .models import Book, Order
+
+
+class BookSerializer(serializers.ModelSerializer):
+    # реализуйте сериализацию объектов модели Book
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    #доп задание
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     representation['orders_count'] = ...
+    #     return representation
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    # добавьте поля модели Order
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    #доп задание
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     representation['books'] = ...
+    #     return representation
---
+++ 3.2/library/main/views.py
@@ -0,0 +1,51 @@
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from main.models import Book, Order
+from main.serializers import BookSerializer
+
+
+@api_view(['GET'])
+def books_list(request):
+    """получите список книг из БД
+    отсериализуйте и верните ответ
+    """
+    books = Book.objects.all()
+    ser = BookSerializer(books, many=True)
+    return Response(ser.data)
+
+
+class CreateBookView(APIView):
+    def post(self, request):
+        # получите данные из запроса
+        book = request.data
+        serializer = BookSerializer(data=book) #передайте данные из запроса в сериализатор
+        if serializer.is_valid(raise_exception=True): #если данные валидны
+            serializer.save()
+            return Response('Книга успешно создана') # возвращаем ответ об этом
+
+
+class BookDetailsView(RetrieveAPIView):
+    # реализуйте логику получения деталей одного объявления
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(UpdateAPIView):
+    # реализуйте логику обновления объявления
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(DestroyAPIView):
+    # реализуйте логику удаления объявления
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    # реализуйте CRUD для заказов
+    ...
